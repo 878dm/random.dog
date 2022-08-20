1f18e12d7aa7b460a6908fd1fa2c160a88429a1d
878 <majidovdiyorbek741@gmail.com>
2022-08-20 10:01:04 +0500

    random cat

--- random_dog.py
+++ random_dog.py
@@ -2,15 +2,20 @@ import requests
 TOKEN='5549407151:AAGVaQx5L2bvwBYnZE3a50yycdBQfPvl1fo'
 
 def send_button(chat_id):
-    button = {
-        'text':'random'
+    button1 = {
+        'text':'random dog'
+    }
+    button2 = {
+        'text':'random cat'
     }
     data = {
         'chat_id':chat_id,
         'text':"random",
         "reply_markup":{
+            "resize_keyboard":True,
             'keyboard':[
-                [button]
+                [button1],
+                [button2]
             ]
         }
     }
@@ -25,7 +30,16 @@ def send_Photo(chat_id):
         'photo':photo
     }
     url=f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
-    print(url)
+    requests.post(url,params=payload)
+
+def send_photocat(chat_id):
+    url =f"https://aws.random.cat/meow"
+    photo = requests.get(url).json()['file']
+    payload={
+        'chat_id':chat_id,
+        'photo':photo
+    }
+    url=f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
     requests.post(url,params=payload)
 
 def get_Updates():
@@ -37,7 +51,6 @@ def get_Updates():
     chat_id = data['message']['chat']['id']
     return update_id, chat_id, text
 
-
 last_update_id = -1
 
 while True :
@@ -46,12 +59,8 @@ while True :
         if text == '/start':
             send_button(chat_id)
 
-        if text == "random":
+        if text == "random dog":
             send_Photo(chat_id)
+        elif text == 'random cat':
+            send_photocat(chat_id)
         last_update_id = update_id
-
-
-
-
-# x=send_button(1066527771)
-# print(x)
